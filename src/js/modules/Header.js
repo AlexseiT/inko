c0c4e5480fffda76c4406dd625d358eb29ec1e78
AlexseiT <alex.todorov@mail.ru>
2023-12-29 12:26:09 +0700

    eddit

--- src/js/modules/Header.js
+++ src/js/modules/Header.js
@@ -2,12 +2,24 @@ class Header {
     constructor(root) {
         this.root = root;
         this.isWhite = this.root.classList.contains('header_white');
-
+        this.scrollPrev = 0;
         this.bindListeners();
     }
-
     bindListeners() {
         //Наложение класса при скролле
+		
+
+        document.addEventListener('scroll', (event) => {
+            let scrolled = window.scrollY;
+        
+            if ( scrolled > 100 && scrolled > this.scrollPrev ) {
+                this.root.classList.add('header__out');
+            } else {
+                this.root.classList.remove('header__out');
+            }
+            this.scrollPrev = scrolled;
+        });
+
         document.addEventListener('scroll', (event) => {
             if(window.scrollY > 50) {
                 this.root.classList.add('header_scroll');
